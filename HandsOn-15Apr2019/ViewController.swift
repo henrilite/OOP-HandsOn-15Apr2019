4219627be70240c87c9b654c628e63f300df838e
Henri Lie Jaya <henrilite@gmail.com>
2019-05-15 13:21:20 +0700

    Initial Commit

---
+++ HandsOn-15Apr2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HandsOn-15Apr2019
+//
+//  Created by Henri Lie Jaya on 15/05/19.
+//  Copyright © 2019 Henri Lie Jaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
